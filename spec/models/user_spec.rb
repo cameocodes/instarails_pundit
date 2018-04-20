4e056f935559ff30cd98e6604e999a70f8544484
Cameo <hello@aeglir.com.au>
2018-04-20 12:05:41 +1000

    Add Devise, testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+require 'bcrypt'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have matching email' do
+    user = User.new
+    user.email = ENV.fetch('USER_EMAIL')
+    expect(user.email).to eq('cameolangford@live.com')
+  end
+
+  # it 'should have matching password' do
+  #   password = 'password123'
+  #   encrypted_password = # password encrytpor
+  #   user = User.new
+  #   user.password = password
+  #   user.save!
+  #   expect(user.encrypted_password).to eq(encrypted_password)
+  # end
 end
